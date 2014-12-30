3a4531ff5ffc1bda3e63074a4570eab5aab61a74
AndyHu <meeasyhappy@gmail.com>
2014-12-30 10:51:38 +0800

    No message.

--- Demo1/Demo1/ViewController.swift
+++ Demo1/Demo1/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     // 重力行为
     var gravity: UIGravityBehavior!;
+    // 碰撞行为
+    var collision: UICollisionBehavior!;
     
     override func viewDidLoad() {
        
@@ -21,14 +23,36 @@ class ViewController: UIViewController {
         let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100));
         square.backgroundColor = UIColor.cyanColor();
         self.view.addSubview(square);
+        
+        
+        // 创建一个长方形View，颜色设置为红色，加入当前的View中
+        let barrier = UIView(frame:CGRect(x: 0, y: 300, width: 140, height: 20));
+        barrier.backgroundColor = UIColor.redColor();
+        self.view.addSubview(barrier);
+        
         // 实例化UIKit物理引擎类，作用于ViewController的View
         animator = UIDynamicAnimator(referenceView: self.view);
         
         // 实例化重力行为类，目前只作用于刚才创建的正方形View
         gravity = UIGravityBehavior(items: [square]);
+        // 角度
+        gravity.angle = 1.6;
+        
+        // 速率
+        //gravity.magnitude = 0.1;
         
         // 将重力行为添加到UIKit物理引擎类中
+        // 实例化碰撞行为类，目前只作用于刚才创建的正方形View
+        collision = UICollisionBehavior(items: [square]);
+        collision.addBoundaryWithIdentifier("barrier", forPath: UIBezierPath(rect: barrier.frame));
+        
+        // 将参考视图的边界作为碰撞边界
+        collision.translatesReferenceBoundsIntoBoundary = true;
+        
+        // 将碰撞行为添加到UIKit物理引擎类中
+        animator.addBehavior(collision);
         animator.addBehavior(gravity);
+        
         
          super.viewDidLoad()
     }
